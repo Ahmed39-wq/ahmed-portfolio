aea9af76c4b784d7d7a6294172035f2ea5abe70f
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-05-26 15:25:36 +0000

    first commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+// import Homepage from "./pages/homepage"
+import Header from "./components/Header/header"
+
+
+const App = () => {
+
+  return (
+      <div>
+        {/* <Homepage /> */}
+        <Header />
+      </div>
+  )
+}
+
+export default App
