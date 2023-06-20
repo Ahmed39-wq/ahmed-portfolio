e3f3359575b9c099f4e152c4acce865b327e85b3
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-06-20 16:51:23 +0000

    edited header component

--- portfolio/src/components/Header/header.tsx
+++ portfolio/src/components/Header/header.tsx
@@ -1,4 +1,5 @@
 import '../Header/header.scss'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -7,10 +8,18 @@ const Header = () => {
         <span>Maya Nelson </span> | Product Manager
       </div>
       <div className="options">
-        <li>About Me</li>
-        <li>Resume</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <NavLink to={"/"} id='nav-links'>
+          About Me
+        </NavLink>
+        <NavLink to={"/resume"} id='nav-links'>
+          Resume
+        </NavLink>
+        <NavLink to={"/projects"} id='nav-links'>
+          Projects
+        </NavLink>
+        <NavLink to={"/contact"} id='nav-links'>
+          Contact
+        </NavLink>
       </div>
     </div>
   )
