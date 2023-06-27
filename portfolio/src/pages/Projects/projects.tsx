6e266ddda2973ccbbd33bee4da04e6c0a183d721
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-06-27 11:01:38 +0000

    working on media query for project page

--- portfolio/src/pages/Projects/projects.tsx
+++ portfolio/src/pages/Projects/projects.tsx
@@ -21,13 +21,15 @@ const Projects = () => {
           <div className="cards">
             <div className="card">
               <div className="side-line"></div>
-              <h3>Project A</h3>
-              <span>Role</span>
-              <p>I'm a paragraph. Click here to add your own text and edit me. It’s 
-                 easy. Just click “Edit Text” or double click me to add your own 
-                 content and make changes to the font. I’m a great place for you to 
-                 tell a story and let your users know a little more about you.
-              </p>
+              <div className="project_text">
+                <h3>Project A</h3>
+                <span>Role</span>
+                <p>I'm a paragraph. Click here to add your own text and edit me. It’s 
+                  easy. Just click “Edit Text” or double click me to add your own 
+                  content and make changes to the font. I’m a great place for you to 
+                  tell a story and let your users know a little more about you.
+                </p>
+              </div>
               <div className="card-sec"></div>
             </div>
             <div className="card">
